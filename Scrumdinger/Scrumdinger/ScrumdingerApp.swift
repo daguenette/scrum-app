9bf7bc81f9334accd579f831e1d942c216de95b5
daguenette <daguenette.data@gmail.com>
2024-10-28 15:13:12 -0400

    first commit

---
+++ Scrumdinger/Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by David-Alexandre Guenette on 2024-10-28.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
